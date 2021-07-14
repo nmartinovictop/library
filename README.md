17ad2a15c9eda9bd815699878db47501ce9c3c1c
Nick Martinovic TOP <nmartinovictop@gmail.com>
2021-07-15 07:48:25 +0900

    Can add a book

--- README.md
+++ README.md
@@ -4,5 +4,18 @@ Library Project from TOP
 Notes:
 - I need to figure out how to create a popup modal when I click on "add book"  (done)
 - I need to figure out how to use javascript with forms (done)
-- I need to create a complete example of what a finished book should look like (sliders, delete, etc) <-- confirm this looks good to me>
-- I will need to create a function to create books by divs
+- I need to create a complete example of what a finished book should look like (sliders, delete, etc) (good enough)
+- I will need to create a function to create books by divs (I can create books on the shelf, no problem)
+- Need to connect the toggle so that it updates whether or not a book is read or not
+
+
+
+
+CAPTURE:
+- Need to be able to delete a book from the shelf by clicking the 'x' icon
+- Need to be show 'pages' text in pages section
+- Need to confirm that you can't add the same book more than once.
+- Need to color the skeleton
+- Need to update the formatting of the modal
+- Need to style the new text
+- Add a pencil edit icon and allow users to edit the book after creation
--- script.js
+++ script.js
@@ -17,19 +17,43 @@ const title = document.querySelector('#title')
 const author = document.querySelector('#author')
 const pages = document.querySelector('#pages')
 const isRead = document.querySelector('#isRead')
+const shelf = document.querySelector('.shelf')
 
+function htmlBook() {
+
+return `<h3 class="book-title"><span class="close-btn-title">×</span></h3><p class="book-author"></p><p class='book-pages'></p><p class='book-isRead'>Read?</p><div class="toggle"><label class="switch">
+    <input type="checkbox">
+    <span class="slider"></span>
+  </label></div>`
+
+}
 
 function runner() {
     myLibrary.push(new Book(title.value,author.value,pages.value,isRead.value));
     
     form.reset()
     modal.style.display = "none"
+    addBookToShelf()
 }
-
 
 form.addEventListener('submit',runner)
 
+function addBookToShelf() { 
+    const div = document.createElement('div')
+    div.classList.add('book')
+    div.innerHTML = htmlBook()
+    div.firstChild.textContent = myLibrary[myLibrary.length-1].title + div.firstChild.textContent
+    div.children[1].textContent = myLibrary[myLibrary.length-1].author
+    div.children[2].textContent = myLibrary[myLibrary.length-1].pages
 
+    shelf.appendChild(div)
+}
+
+function addAllBooksToShelf() {myLibrary.forEach(bookObj => {
+    const div = document.createElement('div')
+    div.classList.add('book')
+    shelf.appendChild(div)
+})}
 
 let modalBtn = document.getElementById("modal-btn")
 let modal = document.querySelector(".modal")
